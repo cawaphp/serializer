871db0d1a0412768807b6a0f495e3bd8440eb172
tchiotludo <tchiot.ludo@gmail.com>
2017-11-09 21:10:35 +0100

    Simple Json serializer that throw exception

---
+++ src/Json.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the Сáша framework.
+ *
+ * (c) tchiotludo <http://github.com/tchiotludo>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace Cawa\Serializer;
+
+abstract class Json
+{
+    /**
+     * @param string $json
+     * @param bool $assoc
+     *
+     * @return mixed
+     */
+    public static function decode(string $json, bool $assoc = true)
+    {
+        $decoded = json_decode($json, $assoc);
+
+        if ((unset) $decoded === $decoded) {
+            throw new \InvalidArgumentException(sprintf(
+                "Json decode failed (%s) with '%s' and data '%s'",
+                json_last_error(),
+                json_last_error_msg(),
+                strlen($json) > 50 ? substr($json, 0, 150) . '...' : $json
+            ));
+        }
+
+        return $decoded;
+    }
+
+    /**
+     * @param $value
+     * @param int $options
+     *
+     * @return string
+     */
+    public static function encode($value, int $options = null) : string
+    {
+        if ($options) {
+            return json_encode($value, $options);
+        } else {
+            return json_encode($value);
+        }
+    }
+}
